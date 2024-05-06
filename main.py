3eb8e42a566f948dead124dd63b4d01da2c6e5c6
atul b <atul.bharadwaj@symphonyai.com>
2024-05-06 23:09:16 +0530

    testing

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-import seaborn as sns
+import pandas as pd
+from eurekaai.components.pipelines import pipeline
+from eurekaai.components.steps import step, Output
+from eurekaai.config.container import ContainerSetting
+from eurekaai.config.kubeflow import KubeflowSettings
+from eurekaai.config.resource import ResourceSettings
+import eurekaai.config.client as client
 
-# Load the built-in 'iris' dataset from seaborn into a DataFrame
-iris_df = sns.load_dataset('iris')
+NAMESPACE = "atul-bharadwaj-1"
+USERNAME = "atul.bharadwaj@symphonyai.com"  # This is the username for the profile you want to use
+PASSWORD = "MTcxNDk4ODczOXxOd3dBTkVnMFVEUTFRVXRTTmxvME4wUlJXa1UyTkVaSVRGSTJNbEpMVnpSVlVrVlNORXhWUlROTlFsQllTMVZZV0ZWRk5VNVpURkU9fK2lGwhrtK99JzZXqETBl0vAKAlSbUNlZkIBlojlawr7"
 
-# Display the first few rows of the DataFrame
-print(f"shape of df is {iris_df.shape}")
-print(iris_df.head())
+kubeflow_setting = KubeflowSettings(username=USERNAME, key=PASSWORD, namespace=NAMESPACE, synchronous=True)
+
+container_settings = ContainerSetting(parent_image="crescendoimages.azurecr.io/eureka_dml_pipeline:v4.7.0.2",
+                                      requirements=["kfp ==1.8.16", "pyarrow",
+                                                    "typing_extensions==4.7.1",
+                                                    "pydantic==1.10.12", "pyiceberg==0.4.0"])
+
+
+@step(enable_cache=False)
+def load_dataset_via_dl() -> None:
+    print("-------hello world------")
+
+
+@pipeline(enable_cache=False, settings={"container": container_settings, "kubeflow": kubeflow_setting})
+def data_library_pipeline(load_dataset_via_dl):
+    load_dataset_via_dl()
+
+
+if __name__ == "__main__":
+    pipeline_instance = data_library_pipeline(
+        load_dataset_via_dl=load_dataset_via_dl()
+    )
+    pipeline_instance.run(unlisted=True)
